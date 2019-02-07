ce99b01bb24630100d4bfc32c0abb3ca453ea42c
Adam Moran <jeffadammoran@gmail.com>
2019-02-07 21:06:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+speakers=[]
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  batch_speakers = []
+  speakers.each do |i| 
+     batch_speakers.push(badge_maker(i))
+  end
+  return batch_speakers
+end
+
+def assign_rooms(speakers)
+  speakers.each {|i| puts "Hello, #{name}! You'll be assigned to room #{speakers.index(i)+1}"}
+end
+
